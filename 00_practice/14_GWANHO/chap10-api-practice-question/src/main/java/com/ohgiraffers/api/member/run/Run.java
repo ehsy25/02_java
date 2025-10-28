ff038c8bbca0a8210448336999cea4d345600f21
Apeirogon99 <apeirogon99@gmail.com>
2025-10-28 18:32:48 +0900

    chap10

--- 00_practice/14_GWANHO/chap10-api-practice-question/src/main/java/com/ohgiraffers/api/member/run/Run.java
+++ 00_practice/14_GWANHO/chap10-api-practice-question/src/main/java/com/ohgiraffers/api/member/run/Run.java
@@ -3,25 +3,33 @@ package com.ohgiraffers.api.member.run;
 import com.ohgiraffers.api.member.model.vo.Member;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Run {
     public static void main(String[] args) {
 
-        Member member1 = new Member(1, "김연아", 165, 47, LocalDate.of(1990, 9, 5));
-        Member member2 = new Member(2, "양세형", 167, 60, LocalDate.of(1985, 8, 18));
-        Member member3 = new Member(3, "김래원", 182, 80, LocalDate.of(1981, 3, 19));
+        String str = "1,김연아,165,47,19900905|2,양세형,167,60,19850818|3,김래원,182,80,19810319";
 
-        Member[] members = new Member[3];
-        members[0] = member1;
-        members[1] = member2;
-        members[2] = member3;
+        String[] infos = str.split("\\|");
 
-        for(int i = 0; i < members.length; i++){
-            System.out.print(members[i].information());
+        Member[] members = new Member[infos.length];
+        for (int i = 0; i < members.length; i++) {
+            String[] part = infos[i].split(",");
 
-            if(i != members.length - 1){
-                System.out.print(" | ");
-            }
+            int no = Integer.parseInt(part[0]);
+            String name = part[1];
+            int height = Integer.parseInt(part[2]);
+            int weight = Integer.parseInt(part[3]);
+
+            int year = Integer.parseInt(part[4].substring(0, 4));
+            int month = Integer.parseInt(part[4].substring(4, 6));
+            int day = Integer.parseInt(part[4].substring(6, 8));
+            LocalDate birth = LocalDate.of(year, month, day);
+            members[i] = new Member(no, name, height, weight, birth);
+        }
+
+        for (Member member : members) {
+            System.out.println(member.information());
         }
     }
 }
