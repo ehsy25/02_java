195366c389419a5cc2be0be6bc135c4072142408
Apeirogon99 <apeirogon99@gmail.com>
2025-10-29 16:09:46 +0900

    chap10, 11

---
+++ 14_GWANHO/study/src/chap10_exception/section02/exception/NegativeException.java
@@ -0,0 +1,13 @@
+package chap10_exception.section02.exception;
+
+/*
+    사용자 정의 예외클래스 만들기 위해서는 Exception 클래스를 상속 받으면 된다.
+    경우에 따라서는 더 상위 타입인 Throwable클래스나 하위 타입의 클래스를 상속받기도한다.
+ */
+
+public class NegativeException extends Exception {
+
+    public NegativeException(String message) {
+        super(message);
+    }
+}
