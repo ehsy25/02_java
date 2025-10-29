ac246c183d718a45a771ac5d64462693165aa462
NoSa1e <cjsp1217@gmail.com>
2025-10-29 17:24:23 +0900

    solve

---
+++ 00_practice/23_JUNGJAEWOO/chap11-exception-practice-question/src/main/java/com/ohgiraffers/exception/user/MissingInfoException.java
@@ -0,0 +1,11 @@
+package com.ohgiraffers.exception.user;
+
+public class MissingInfoException extends Exception{
+    public MissingInfoException(String message) {
+        super(message);
+    }
+
+    public MissingInfoException() {
+
+    }
+}
