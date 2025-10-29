694cd8c46d06abc6824f24dd4d3f1bf7e278428a
Gyuwon-Jung <licmm0520@gmail.com>
2025-10-29 16:46:33 +0900

    예외처리 문제 풀이 완

---
+++ 00_practice/22_JUNGGYUWON/chap11-exception-practice-question/src/main/java/com/ohgiraffers/exception/number/NumberRangeException.java
@@ -0,0 +1,7 @@
+package com.ohgiraffers.exception.number;
+
+public class NumberRangeException extends Exception{
+    public NumberRangeException() {
+        System.out.println("1부터 100사이의 값이 아닙니다.");
+    }
+}
